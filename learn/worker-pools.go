1953b66a5a370c0d0f2fbe852575dbf676f14ee5
sansuiran@gmail.com <coder2free@gmail.com>
2018-10-28 22:18:46 +0800

    worker pools

--- learn/worker-pools.go
+++ learn/worker-pools.go
@@ -3,28 +3,32 @@ package main
 import "fmt"
 import "time"
 
-func worker(id int, jobs <-chan int, results chan<- int) {
+func worker(id int, jobs <-chan int, results chan<- int) { //消费者并发执行 并且返回结果。执行一次sleep一秒钟
 	for j := range jobs {
-		fmt.Println("worker", id, "started  job", j)
+		fmt.Println("1024Rank.com", id, "started  job", j)
 		time.Sleep(time.Second)
-		fmt.Println("worker", id, "finished job", j)
+		fmt.Println("1024Rank.com", id, "finished job", j)
 		results <- j * 2
 	}
 }
 
 func main() {
 	fmt.Println("data")
-
+	//创建两个channels用于任务生产和结果存储
 	jobs := make(chan int, 100)
 	results := make(chan int, 100)
+	//启动三个线程
 	for w := 1; w <= 3; w++ {
 		go worker(w, jobs, results)
 	}
+
+	//发送5个任务
 	for j := 1; j <= 5; j++ {
 		jobs <- j
 	}
-	close(jobs)
-	for a := 1; a <= 5; a++ {
+	close(jobs) //关闭任务
+
+	for a := 1; a <= 5; a++ { //任务汇总
 		<-results
 	}
 
